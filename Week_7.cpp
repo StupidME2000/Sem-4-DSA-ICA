7a1b8d988073bf7a9b2bcc52abc50aaf2c33e477
StupidME2000 <95670058+StupidME2000@users.noreply.github.com>
2023-05-09 12:12:08 +0530

    Update Week_7.cpp

--- Week_7.cpp
+++ Week_7.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// structure for a node in the binary search tree
 struct node {
     int key;
     struct node *left, *right;
@@ -15,7 +16,7 @@ struct node* New_node(int data){
     return temp;
 }
 
-//Returns the node with minimum key value
+// find the node with minimum key value in a subtree
 struct node* minimum(struct node* node){
     struct node* NowNode = node;
     while (NowNode->left != NULL){
@@ -36,10 +37,8 @@ void traverseInOrder(struct node *root) {
     }
 }
 
-
-// Insert a node
+// Insert a node into the binary search tree
 struct node *insertNode(struct node *node, int key) {
-
     if (node == NULL){
         return New_node(key);
     }
@@ -52,8 +51,7 @@ struct node *insertNode(struct node *node, int key) {
     return node;
 }
 
-
-// Deleting a node
+// Deleting a node from the binary search tree
 struct node *deleteNode(struct node *root, int data) {
     if (root ==NULL){
         return root;
@@ -66,32 +64,28 @@ struct node *deleteNode(struct node *root, int data) {
 
     }
     else{
-        //no child
+        // no child
         if (root -> left == NULL and root -> right == NULL){
             return NULL;
         }
-
-            //only right Child
+        // only right child
         else if (root -> left == NULL){
             struct node* temp = root -> right;
             free(root);
             return temp;
         }
-
-            //only left child
+        // only left child
         else if (root -> right == NULL){
             struct node* temp = root -> left;
             free(root);
             return temp;
         }
-
-            //Node with two children
+        // node with two children
         else{
             struct node* temp = minimum(root->right);
             root -> key = temp -> key;
             root -> right = deleteNode(root->right, temp -> key);
         }
-
     }
     return root;
 }
@@ -104,23 +98,24 @@ int main() {
     int operand;
     cin >> operation;
 
+    // loop through the input operations
     while (operation != -1) {
         switch(operation) {
-            case 1: // insert
+            case 1: // insert a node into the binary search tree
                 cin >> operand;
                 root = insertNode(root, operand);
                 cin >> operation;
                 break;
-            case 2: // delete
+            case 2: // delete a node from the binary search tree
                 cin >> operand;
                 root = deleteNode(root, operand);
                 cin >> operation;
                 break;
             default:
-                cout << "Invalid Operator!\n";
+                cout << "Invalid Operator!\n"; // if the input operation is not 1 or 2, print an error message
                 return 0;
         }
     }
 
-    traverseInOrder(root);
+    traverseInOrder(root); // print the inorder traversal of the binary search tree
 }
